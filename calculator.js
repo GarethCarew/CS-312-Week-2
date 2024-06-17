99fda4fb3ee97573362e8f28cb30b0d89f80e633
Gareth Carew <agarethcarew@gmail.com>
2024-06-16 21:34:34 -0700

    Added part 2

--- calculator.js
+++ calculator.js
@@ -1,8 +1,23 @@
 const express = require("express")
+const bodyParser = require("body-parser")
 const app = express()
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get("/", (req, res) => {
   res.send("Hello World!")
+})
+
+app.get("/bmicalculator", (req, res) => {
+  res.sendFile(__dirname + "/bmiCalculator.html")
+})
+
+app.post("/bmicalculator", (req, res) => {
+  var height = req.body.height
+  var weight = req.body.weight
+
+  var BMI = 703 * (weight / (height * height))
+  BMI = BMI.toFixed(1)
+  res.send("Your BMI is " + BMI)
 })
 
 app.listen(3000, () => {
